c8279b6852023e03052ac08aac791a319ec187ba
Christopher Goes <ghostofgoes@gmail.com>
2016-09-06 14:26:17 -0700

    Updated submission URL

--- curlsubmit.sh
+++ curlsubmit.sh
@@ -4,4 +4,4 @@ ass=$2
 file=$3
  
 # adjust the next line for class and semester
-curl -F student=$name -F assignment="CS472 F16 Assignment $ass" -F "submittedfile=@$file" "http://ec2-54-200-16-181.us-west-2.compute.amazonaws.com/cgi-bin/fileCapture.py"
+curl -F student=$name -F assignment="CS472 F16 Assignment $ass" -F "submittedfile=@$file" "http://ec2-52-89-93-46.us-west-2.compute.amazonaws.com/cgi-bin/fileCapture.py"
