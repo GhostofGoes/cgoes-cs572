d909ab4c16d7b46ce8715b055383c6b78f8e084a
Christopher Goes <ghostofgoes@gmail.com>
2016-09-23 23:12:36 -0700

    Got tables working finally
    Should've been at this point last weekend, meh, distractions.
    I also would've rage quit less if I weren't so bad at figuring out
    string processing in c++.
    python is simpler, and according to a stack overflow post, faster at it.

--- 2/decipher.cpp
+++ 2/decipher.cpp
@@ -9,31 +9,80 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 #include "rand.h"
 #define TESTING 1
 using namespace std;
 
-double eTable[26][26];
-double cTable[26][26];
+double eTable[26][26];		// English contact table
+double cTable[26][26];		// Cipher contact table
 
-void initTable();
-string decode(string message, char * key);
+inline int encode( int index, char * key ); // index + key -> index
+double eFitness( char * key );	// Euclidean distance
+double bFitness( char * key );	// Bhatthacaryya distance
+
+void initETable();
+void initCTable( string ciphertext );
+inline int convert( char c );
+void printTable( double table[][26], string name );
 
 int main() {
-	string msg;
+	string ciphertext, temp, plaintext;
 	char key[26];
 
-	initRand();  // Initialize random number generator
-	initTable();
-	
+	initRand();  	// Initialize random number generator
+	initETable();	// Initialize the English contact table
+	while( cin >> temp ) { ciphertext += temp; } // Input the ciphertext 
+	if(TESTING) { cout << "ciphertext: " << ciphertext << endl; }
+	initCTable(ciphertext);
+	// (c++ is so freaking annoying when it comes to string processing...)
+
+	if(TESTING) {
+		printTable(eTable, "eTable post-init");
+		printTable(cTable, "cTable post-init");
+	}
+
 	cout << "** goes " << key << endl;
 	return 0;
 } // end main
 
 
-void initTable() {
+// Uses index to select character from key for fitness evaluation
+// NOTE: does not bounds check, if theres bug it'll probably happen here
+inline int encode( int index, char * key ) {
+	return (int)(convert(key[index]));  // Converts char to int (ASCII 'a' - 97 = 0)
+} // end encode
+
+// Fitness function using Euclidean distance
+// SMALLER IS BETTER FOR THIS FITNESS
+double eFitness( char * key ) {
+	double fit = 0.0; // Fitness
+	for( int i = 0; i < 26; i++ ) {
+		for( int j = 0; j < 26; j++ ) {
+			fit += pow(( eTable[i][j] - cTable[encode(i, key)][encode(j, key)] ), 2);
+		}
+	}
+	return fit;
+} // end eFitness
+
+
+// Fitness function using Bhatthacaryya distance
+// BIGGER IS BETTER FOR THIS FITNESS (oh yeah)
+double bFitness( char * key ) {
+	double fit = 0.0; // Fitness
+	for( int i = 0; i < 26; i++ ) {
+		for( int j = 0; j < 26; j++ ) {
+			fit += sqrt(eTable[i][j] * cTable[encode(i, key)][encode(j, key)]);
+		}
+	}
+	return fit;
+} // end bFitness
+
+
+// Initializes the English contact table from a file
+void initETable() {
 	char a = 0, b = 0;
-	double num = 0.0, sum = 0.0;
+	double num = 0, sum = 0;
 	ifstream inf("freq.txt");
 
 	// Initialize the table
@@ -47,27 +96,59 @@ void initTable() {
 	while(inf) {
 		inf >> a >> b >> num;
 		sum += num;
-		//cout << "a: " << a - 97 << " |b: " << b - 97 << " |num: " << num << endl;
-		eTable[a - 97][b - 97] = num;
+		eTable[convert(a)][convert(b)] = num;
 	}
-	//cout << sum << endl;
+	//printTable(eTable, "eTable: post-load");
 
 	// Normalize the frequencies
 	for(int i = 0; i < 26; i++) {
 		for(int j = 0; j< 26; j++) {
-			//cout << eTable[i][j] << endl;;
 			if(eTable[i][j] != 0)
 				eTable[i][j] /= sum;
-			//cout << eTable[i][j] << endl;;
+		}
+	}
+	//printTable(eTable, "eTable: post-normalize");
+	inf.close();
+ } // end initETables
+
+// Initializes the Ciphertext contact table by counting occurances of pairs of letters in the ciphertext
+void initCTable( string ciphertext ) {
+	// Initialize the table
+	for(int i = 0; i < 26; i++) {
+		for(int j = 0; j < 26; j++) {
+			cTable[i][j] = 0;
 		}
 	}
 
-	inf.close();
+	// Count the number of times each pair of characters occurs in the ciphertext input
+	// length - 1 = total number of pairs in the ciphertext
+	for( int i = 0; i < ciphertext.length() - 1; i++ ) {
+		cTable[convert(ciphertext[i])][convert(ciphertext[i + 1])] += 1;
+	}
+	//printTable(cTable, "cTable: post-load");
+	// Normalize the frequencies
+	for(int i = 0; i < 26; i++) {
+		for(int j = 0; j< 26; j++) {
+			if(cTable[i][j] != 0)
+				cTable[i][j] /= (double)(ciphertext.length() - 1); 
+		}
+	}
+	//printTable(cTable, "cTable: post-normalize");
+} // end initCTable
+
+
+// Converts a character to an int to be used to index a table
+inline int convert( char c ) {
+	return (int)(c - 97);
 }
 
-string decode( string message, char * key ) {
-	string decoded;
-
-
-	return decoded;
+// Print 26 x 26 table
+void printTable( double table[][26], string title = "Table" ) {
+	cout << "\n" << "** " << title << " **" << endl;
+	for( int i = 0; i < 26; i++ ) {
+		for( int j = 0; j < 26; j++ ) {
+			cout << table[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
