e3c429fb9af8f981e5bbd26ffa5ccc9a7fa0bb03
Christopher Goes <ghostofgoes@gmail.com>
2016-09-28 18:52:23 -0700

    Added time + organize
    Added time to test scripts.
    Organized runs into folders so not spamming the hw dir.

--- 2/iter_test.sh
+++ 2/iter_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 outfile=$(date +%Y-%m-%d-%H-%M-%S)_decipher_iter-test.run
 for file in codes/*.code; do
-    printf "\n\nTesting %s...\n" "$file" >> "$outfile"
+    printf "\n\nTesting %s...\n" "$file" >> "runs_iter/$outfile"
     for i in $(seq 1 10); do
-        ./decipher < "$file" >> "$outfile"
+        (time ./decipher < "$file") &>> "runs_iter/$outfile"
     done
 done
--- 2/test.sh
+++ 2/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 outfile=$(date +%Y-%m-%d-%H-%M-%S)_decipher.run
 for file in codes/*.code; do
-    printf "\n\nTesting %s...\n" "$file" >> "$outfile"
-    ./decipher < "$file" >> "$outfile"
+    printf "\n\nTesting %s...\n" "$file" >> "runs_normal/$outfile"
+    (time ./decipher < "$file") &>> "runs_normal/$outfile"
 done
